73cdf8d682be10eb2f2cf08a7527c23b4106d4bf
ahmediosDeveloper <ahmedaeg1993@hotmail.com>
2024-04-16 13:33:15 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonIVSPlayer",
-            url:"https://player.live-video.net/1.27.0/AmazonIVSPlayer.xcframework.zip",
+            url:"https://github.com/ahmediosDeveloper/AmazonIVSPlayer/releases/download/1.27.0/AmazonIVSPlayer.xcframework.zip",
             checksum: "07a3388dc52cf367fd08892eca907385c8a0552b489f2eb49e811aaf37513bf6"
         )
     ]
